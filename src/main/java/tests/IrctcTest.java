324d45fc4f3a65e6a69e49540a30fcc259dba91b
shubham joshi <shubhamjoshi5112@gmail.com>
2023-11-30 15:21:20 +0530

    deleted idea folder

--- src/main/java/tests/IrctcTest.java
+++ src/main/java/tests/IrctcTest.java
@@ -12,18 +12,15 @@ public class IrctcTest extends BaseTest {
 
     IRCTCPage irctcPage = new IRCTCPage();
     CommonPage commonPage = new CommonPage();
-
     @Test
     public void irctcTest1() {
         Map<String, String> values = commonPage.readExcel("IRCTC_TC_01");
         commonPage.launchUrl();
         irctcPage.clickOnFromTextBox();
         irctcPage.getFromTextBoxLocators();
-        // Assersion
+        // Asserstion
         irctcPage.assertionForgetBookTicketUniqueLocators(values.get("UniqueLocatorCSS"),values.get("UniqueLocatorXpath"));
-
     }
-
     @Test
     public void irctcTest2()  {
         Map<String, String> values = commonPage.readExcel("IRCTC_TC_23");
