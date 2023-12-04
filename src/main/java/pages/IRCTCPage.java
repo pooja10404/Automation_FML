ea3b9d201fe0ce92f7c090efb6043512a0d29e3e
nayansi <nayansipurwar.es@gmail.com>
2023-12-04 19:08:04 +0530

    updated test cases

--- src/main/java/pages/IRCTCPage.java
+++ src/main/java/pages/IRCTCPage.java
@@ -12,53 +12,37 @@ public class IRCTCPage extends SeleniumUtility{
 
     CommonPage commonPage = new CommonPage();
     public String getFromTextBoxLocatorsXpath = "//p-autocomplete[@id='origin']//input[@aria-autocomplete='list']";
-
     public String ArrowTextLocatorCss= "span[class='fa fa-angle-up ng-star-inserted']";
-
     public String getFromBhopalLocatorCss= "div[class='ng-star-inserted']:nth-of-type(1)>div[class*='form-group']:nth-of-type(1) div[class*='white-back']:nth-of-type(3)>div[class*='col-xs-']:nth-of-type(1)";
-
-
     public String getDateTextLocatorCss= "div[class*='row'] > span > strong";
     public String getArrowTextLocatorCss= "div[class*='col-lg-']:nth-of-type(2) button[class='btn_Tab']:nth-of-type(2) span[class='fa fa-angle-down ng-star-inserted']";
-
     public String getIrctcWalletLocatorCss="div[class*='col-lg-']:nth-of-type(3) button[class='btn_Tab']:nth-of-type(1)>span[class='footer_headings']";
     public String getIrctcOtherTextLocatorCss="div[class='border-all ng-star-inserted'] p-panel[class*='ng-tns-c'] table[class*='ng-tns-c'] span[class='t-t-O']";
-
+    public String irctcMovetoElement="div[class*='col-lg-']:nth-of-type(2) button[class='btn_Tab']:nth-of-type(5)";
     public void clickOnFromTextBox() {
         driver.findElement(By.xpath(getFromTextBoxLocatorsXpath)).click();
     }
-
     public void clickOnBhopalText() {
         driver.findElement(By.cssSelector(getFromBhopalLocatorCss)).click();
     }
-
     public void clickOnDateText() {
         driver.findElement(By.cssSelector(getDateTextLocatorCss)).click();
     }
-
     public void clickOnFooterLink() throws InterruptedException {
-        String Element ="div[class*='col-lg-']:nth-of-type(2) button[class='btn_Tab']:nth-of-type(5)";
-        SeleniumUtility.waitForElementVisibility(Element,80);
-        moveToElement(Element);
+        waitForElementVisibility(irctcMovetoElement,80);
+        moveToElement(irctcMovetoElement);
         driver.findElement(By.cssSelector(getArrowTextLocatorCss)).click();
     }
-
     public void irctcWalletLink() throws InterruptedException {
-        String Element ="div[class*='col-lg-']:nth-of-type(2) button[class='btn_Tab']:nth-of-type(5)";
-        SeleniumUtility.waitForElementVisibility(Element,80);
-//        moveToElement(Element);
+        waitForElementVisibility(irctcMovetoElement,80);
         commonPage.rightClickOnFindMyLocator(getIrctcWalletLocatorCss);
     }
-
     public void irctcOtherText() throws InterruptedException {
         commonPage.rightClickOnFindMyLocator(getIrctcOtherTextLocatorCss);
     }
-
-
     public void clickOnArrowButton() throws InterruptedException {
-        String Element ="div[class*='col-lg-']:nth-of-type(2) button[class='btn_Tab']:nth-of-type(5)";
-        SeleniumUtility.waitForElementVisibility(Element,50);
-        moveToElement(Element);
+        waitForElementVisibility(irctcMovetoElement,50);
+        moveToElement(irctcMovetoElement);
         driver.findElement(By.cssSelector(getArrowTextLocatorCss)).click();
         commonPage.rightClickOnFindMyLocator(ArrowTextLocatorCss);
     }
