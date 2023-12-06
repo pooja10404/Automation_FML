6049e9297551c1d08ab995bdd7f65ef016aa3aac
nayansi <nayansipurwar.es@gmail.com>
2023-12-06 16:07:52 +0530

    added test cases

---
+++ src/main/java/pages/UitRgpvPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static base.BaseTest.driver;
+
+public class UitRgpvPage extends SeleniumUtility{
+    CommonPage commonPage = new CommonPage();
+
+    public String getArrowCss = "div[class*='col-lg-']:nth-of-type(1)>div[class='footer_col'] ul[class='list-item']>i[class='la la-angle-right']:nth-of-type(1)";
+    public String closeButton= "button[class='btn btn-secondary btn-sm']";
+    public String moveTolastElement="div[class='text_center'] a:nth-of-type(1)";
+
+    public void ArrowButton() {
+        CloseButton();
+        scrollTillEndOfPage();
+        driver.findElement(By.cssSelector(getArrowCss)).click();
+    }
+
+    public void CloseButton() {
+        waitForElementVisibility(closeButton, 20);
+        driver.findElement(By.cssSelector(closeButton)).click();
+    }
+
+}
