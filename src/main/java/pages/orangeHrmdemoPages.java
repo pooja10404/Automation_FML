62ee41733383baf7d31e1735fe28c19673e2db7f
shubham joshi <shubhamjoshi5112@gmail.com>
2023-12-18 19:02:09 +0530

    new test case added

---
+++ src/main/java/pages/orangeHrmdemoPages.java
@@ -0,0 +1,54 @@
+package pages;
+import org.openqa.selenium.By;
+
+import static base.BaseTest.driver;
+public class orangeHrmdemoPages extends SeleniumUtility{
+    CommonPage commonpage=new CommonPage();
+    public String clickOnlogo = "img[alt='company-branding']";
+    public String clickOnHrmLinkReDirect = "p[class='oxd-text oxd-text--p orangehrm-copyright'] > a";
+    public String clickOnLoginText ="h5[class*='oxd-text']";
+    public String clickOnTextOfUsername ="p[class='oxd-text oxd-text--p']:nth-of-type(1)";
+    public String clickOnTheUsername ="input[name='username']";
+    public String clickOnThePassword ="input[name='password']";
+    public String clickOnTheLogin ="button[class*='oxd-button']";
+    public String clickOnDashboard =" h6[class*='oxd-text']";
+    public String clickOnProfile ="img[class='oxd-userdropdown-img']";
+    public String clickOnTheHlep ="i[class='oxd-icon bi-question-lg']";
+    public String clickOnTheAdmin ="li[class='oxd-main-menu-item-wrapper']:nth-of-type(1) span[class='oxd-text oxd-text--span oxd-main-menu-item--name']";
+    public String clickOnPunchTime="div[class='orangehrm-todo-list-item']:nth-of-type(1) > p[class='oxd-text oxd-text--p']";
+
+
+    public void ClickOnOrangeHrmLogo() {
+        driver.findElement(By.cssSelector(clickOnlogo)).click();
+    }
+
+    public void ClickOnHrmLinkReDirct() {
+        driver.findElement(By.cssSelector(clickOnHrmLinkReDirect)).click();
+    }
+
+    public void ClickOnTheLoginText() {
+        driver.findElement(By.cssSelector(clickOnLoginText)).click();
+    }
+
+    public void ClickOnTheUsernameText() {
+        driver.findElement(By.cssSelector(clickOnTextOfUsername)).click();
+    }
+    public void ClickOnTheUsername() {
+        driver.findElement(By.cssSelector(clickOnTheUsername)).click();
+    }
+    public void Login()
+    {
+        driver.findElement(By.cssSelector(clickOnTheUsername)).sendKeys("Admin");
+        driver.findElement(By.cssSelector(clickOnThePassword)).sendKeys("admin123");
+        driver.findElement(By.cssSelector(clickOnTheLogin)).click();
+    }
+    public void ClickOnTheDashboard() {
+        driver.findElement(By.cssSelector(clickOnDashboard)).click();
+    }
+    public void ClickOnTheProfilePhoto() {
+        driver.findElement(By.cssSelector(clickOnProfile)).click();
+    }
+    public void ClickOnTheHelpPopUp() {
+        commonpage.rightClickOnFindMyLocator(clickOnTheHlep);
+    }
+}
---
+++ src/main/java/tests/orangeHrmDemoTest.java
@@ -0,0 +1,96 @@
+package tests;
+import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.CommonPage;
+import pages.orangeHrmdemoPages;
+
+import java.util.Map;
+
+public class orangeHrmDemoTest extends BaseTest {
+    orangeHrmdemoPages orangehrm = new orangeHrmdemoPages();
+
+    @Test(priority = 1)
+    public void orangehrmlive_TC_01() {
+        CommonPage commonPage = new CommonPage();
+        Map<String, String> values = commonPage.readExcel("orangehrmlive_TC_01");
+        commonPage.launchUrl();
+        orangehrm.ClickOnOrangeHrmLogo();
+        // Asserstion
+        commonPage.assertionForUniqueLocator(values.get("UniqueLocatorCSS"), values.get("UniqueLocatorXpath"));
+    }
+
+    @Test(priority = 2)
+    public void orangehrmlive_TC_02() {
+        CommonPage commonPage = new CommonPage();
+        Map<String, String> values = commonPage.readExcel("orangehrmlive_TC_02");
+        commonPage.launchUrl();
+        orangehrm.ClickOnHrmLinkReDirct();
+        // Asserstion
+        commonPage.assertionForUniqueLocator(values.get("UniqueLocatorCSS"), values.get("UniqueLocatorXpath"));
+        commonPage.assertionForWithoutIndexLocator(values.get("WithoutIndexCSS"), values.get("WithoutIndexXpath"));
+        commonPage.assertionForTextLocator(values.get("TextLocatorCSS"), values.get("TextLocatorXpath"));
+    }
+
+    @Test(priority = 3)
+    public void orangehrmlive_TC_03() {
+        CommonPage commonPage = new CommonPage();
+        Map<String, String> values = commonPage.readExcel("orangehrmlive_TC_03");
+        commonPage.launchUrl();
+        orangehrm.ClickOnTheLoginText();
+        // Asserstion
+        commonPage.assertionForUniqueLocator(values.get("UniqueLocatorCSS"), values.get("UniqueLocatorXpath"));
+    }
+
+    @Test(priority = 4)
+    public void orangehrmlive_TC_04() {
+        CommonPage commonPage = new CommonPage();
+        Map<String, String> values = commonPage.readExcel("orangehrmlive_TC_04");
+        commonPage.launchUrl();
+        orangehrm.ClickOnTheUsernameText();
+        // Asserstion
+        commonPage.assertionForUniqueLocator(values.get("UniqueLocatorCSS"), values.get("UniqueLocatorXpath"));
+    }
+
+    @Test(priority = 5)
+    public void orangehrmlive_TC_05() {
+        CommonPage commonPage = new CommonPage();
+        Map<String, String> values = commonPage.readExcel("orangehrmlive_TC_05");
+        commonPage.launchUrl();
+        orangehrm.ClickOnTheUsername();
+        // Asserstion
+        commonPage.assertionForUniqueLocator(values.get("UniqueLocatorCSS"), values.get("UniqueLocatorXpath"));
+    }
+
+    @Test(priority = 6)
+    public void orangehrmlive_TC_06() {
+        CommonPage commonPage = new CommonPage();
+        Map<String, String> values = commonPage.readExcel("orangehrmlive_TC_06");
+        commonPage.launchUrl();
+        orangehrm.Login();
+        orangehrm.ClickOnTheDashboard();
+        // Asserstion
+        commonPage.assertionForUniqueLocator(values.get("UniqueLocatorCSS"), values.get("UniqueLocatorXpath"));
+    }
+
+    @Test(priority = 7)
+    public void orangehrmlive_TC_07() {
+        CommonPage commonPage = new CommonPage();
+        Map<String, String> values = commonPage.readExcel("orangehrmlive_TC_07");
+        commonPage.launchUrl();
+        orangehrm.Login();
+        orangehrm.ClickOnTheProfilePhoto();
+        // Asserstion
+        commonPage.assertionForUniqueLocator(values.get("UniqueLocatorCSS"), values.get("UniqueLocatorXpath"));
+    }
+
+    @Test(priority = 8)
+    public void orangehrmlive_TC_08() {
+        CommonPage commonPage = new CommonPage();
+        Map<String, String> values = commonPage.readExcel("orangehrmlive_TC_08");
+        commonPage.launchUrl();
+        orangehrm.Login();
+        orangehrm.ClickOnTheHelpPopUp();
+        // Asserstion
+        commonPage.assertionForUniqueLocator(values.get("UniqueLocatorCSS"), values.get("UniqueLocatorXpath"));
+    }
+}
